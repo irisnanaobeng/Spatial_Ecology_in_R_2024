bbac15631d676add3848b9eec40200a6c75cee31
irisnanaobeng <irisnana.obeng@studio.unibo.it>
2025-02-10 17:10:33 +0100

    Update 01_my_first_R_script.R

--- 01_my_first_R_script.R
+++ 01_my_first_R_script.R
@@ -1,1 +1,27 @@
-
+> 2 + 3 
+[1] 5
+> cato = 2 + 3 
+> cato
+[1] 5
+> maria = 4 + 2
+> maria
+[1] 6
+> cato + maria 
+[1] 11
+> cato ^ maria
+[1] 15625
+> (cato + maria) ^ cato
+[1] 161051
+> andrea = c(10,15,20,50,70)
+> andrea
+[1] 10 15 20 50 70
+> sofia = c(100,200,300,400,500)
+> sofia
+[1] 100 200 300 400 500
+> plot(sofia, andrea)
+> plot(sofia, andrea, pch=19)
+> plot(sofia, andrea, pch=19, cex=2)
+> plot(sofia, andrea, pch=19, cex=0.5)
+> plot(sofia, andrea, pch=19, cex=.5)
+> plot(sofia, andrea, pch=19, cex=2, col="blue")
+> plot(sofia, andrea, pch=19, cex=2,col="blue", xlab="CO2", ylab="amount of fruits")
